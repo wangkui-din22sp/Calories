304508c5a8b912e2bc284a6ccf4cddc582c4feee
wangkui-din22sp <c3waku00@students.oamk.fi>
2024-02-27 19:26:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/calories/MainActivity.kt
@@ -0,0 +1,203 @@
+package com.example.calories
+
+import android.graphics.drawable.Icon
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
+import com.example.calories.ui.theme.CaloriesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CaloriesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CalorieScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun  CalorieScreen () {
+    var weightInput: String by remember { mutableStateOf(value="") }
+    var male by remember { mutableStateOf(true) }
+    var intensity by remember { mutableStateOf(1.3f) }
+    var result by remember { mutableStateOf(0) }
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Heading(title = "Calories")
+        WeightField(weightInput = weightInput, onValueChange = { weightInput = it })
+        GenderChoice(male=male, setGenderMale = {male =it})
+        IntensityList(onclick = {intensity = it.toFloat()})
+        Text(
+            text = result.toString(),
+            color = MaterialTheme.colorScheme.secondary,
+            fontWeight = FontWeight.Bold,
+        )
+
+    }
+}
+
+@Composable
+fun Heading(title: String) {
+    Text(
+        text = title,
+        fontSize = 24.sp,
+        textAlign = TextAlign.Center,
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 16.dp, bottom = 16.dp)
+
+
+    )
+}
+
+@Composable
+fun WeightField(weightInput: String, onValueChange: (String) -> Unit) {
+    OutlinedTextField(
+        value = weightInput,
+        onValueChange = onValueChange,
+        label = { Text("Enter Weight (kg)") },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun GenderChoice(male: Boolean, setGenderMale: (Boolean) -> Unit) {
+    Column(
+        modifier = Modifier.selectableGroup()
+
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            RadioButton(
+                selected = male,
+                onClick = { setGenderMale(true) }
+            )
+            Text(text = "Male")
+
+        }
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            RadioButton(
+                selected = !male,
+                onClick = { setGenderMale(false) }
+            )
+            Text(text = "Female")
+
+        }
+    }
+}
+
+@Composable
+fun IntensityList(onclick: (String) -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedText by remember { mutableStateOf("Light") }
+    var textFieldSize by remember { mutableStateOf(Size.Zero) }
+    val items = listOf("Light", "Usual", "Moderate", "Hard", "Very Hard")
+
+    val icon = if (expanded) {
+        Icons.Filled.KeyboardArrowUp
+    } else {
+        Icons.Filled.KeyboardArrowDown
+    }
+
+    Column {
+        OutlinedTextField(
+            readOnly = true,
+            value = selectedText,
+            onValueChange = { selectedText = it },
+            label = { Text("Select Intensity") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier
+                .fillMaxWidth()
+                .onGloballyPositioned { coordinates ->
+                    textFieldSize = coordinates.size.toSize()
+                },
+            trailingIcon = {
+                    Icon(icon, "contentDescription", Modifier.clickable { expanded = !expanded })
+
+            }
+
+        )
+        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false },
+            modifier = Modifier.width(with(LocalDensity.current) { textFieldSize.width.toDp() })
+        ) {
+            items.forEach { label ->
+                DropdownMenuItem(
+                    text = { Text(label) },
+                    onClick = {
+                    selectedText = label
+                    var intensity: Float = when (label) {
+                        "Light" -> 1.3f
+                        "Usual" -> 1.5f
+                        "Moderate" -> 1.7f
+                        "Hard" -> 2f
+                        "Very Hard" -> 2.2f
+                        else -> 0.0f
+                    }
+
+                    expanded = false
+                })
+            }
+
+            
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    CaloriesTheme {
+        CalorieScreen()
+    }
+}
